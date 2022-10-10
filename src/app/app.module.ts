d2f9dc6e65f96b1a0172b8230f54d930ae389bd5
Amalpm7 <amal.p@nestgroup.net>
2022-10-10 11:15:43 +0530

    deletedonor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { AddDonorComponent } from './add-donor/add-donor.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SearchdonorComponent } from './searchdonor/searchdonor.component';
+import { DeletdonorComponent } from './deletdonor/deletdonor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddDonorComponent,
     NavbarComponent,
-    SearchdonorComponent
+    SearchdonorComponent,
+    DeletdonorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/deletdonor/deletdonor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deletdonor',
+  templateUrl: './deletdonor.component.html',
+  styleUrls: ['./deletdonor.component.css']
+})
+export class DeletdonorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
