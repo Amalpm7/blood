c477fe1b6729663b871f8b2319814df5f0390685
Amalpm7 <amal.p@nestgroup.net>
2022-10-14 09:10:43 +0530

    form2

--- src/app/deletdonor/deletdonor.component.ts
+++ src/app/deletdonor/deletdonor.component.ts
@@ -8,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class DeletdonorComponent implements OnInit {
 
   constructor() { }
+  donorId=""
+  readValue=()=>{
+    let data={
+      "donorId":this.donorId
+
+    }
+    console.log(data)
+  }
 
   ngOnInit(): void {
   }
